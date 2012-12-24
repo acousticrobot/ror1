41bd9323121a0485faf4339eef0d6c674f3a8060
Jonathan Gabel <post@jonathangabel.com>
2012-12-24 12:24:36 -0500

    reinstall from base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ror1::Application.config.secret_token = '01c70740e82218c4f4975318554bd39004d9949641687c56a8d082f775d76cddcb67a18c5b3a49209fa0c5e58c008d5ce774a995396a94b3dee693c659445821'
