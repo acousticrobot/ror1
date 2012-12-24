c463f684e2d3f48f08573700ba54f463acfc6ced
Jonathan Gabel <post@jonathangabel.com>
2012-12-24 12:12:56 -0500

    reset from scaffolding test

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = 'ffffd85a4dc3c23ae2e4582250a31540f2537ecf4c7841bc277bb7faa04c3531cbbaaea8f5ed6b9e300b0c47aa53eb17834e474875d20a879278085f0969df53'
